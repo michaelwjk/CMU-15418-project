d6520fc50c48d2b932ce2d4aa02d62bdae58efff
Jingkang Wang <jingkangwang@Jingkangs-MacBook-Pro.local>
2020-04-21 21:45:55 -0400

    Add generate graph

--- data/generate.py
+++ data/generate.py
@@ -1,13 +1,32 @@
-"""
-    A naive version, need to be modified
-"""
+import random
 
-total_vertex = 100
+total_vertex = 10000
 f = open("./test.txt", "w")
-f.write("100\n")
+f.write("{}\n".format(total_vertex))
 
-for i in range(100):
-    line = "{},{},{}\n"
-    f.write(line.format(i, 1, (i+1)%100))
+total_map = {}
+
+for i in range(total_vertex):
+    total_map[i] = set();
+
+for i in range(total_vertex):
+    num = random.randint(1, total_vertex / 10)
+    if num <= len(total_map[i]):
+        continue
+    
+    for n in range(num):
+        neighbor = random.randint(0, total_vertex-1)
+        if neighbor != i:
+            total_map[i].add(neighbor)
+            total_map[neighbor].add(i)
+
+
+for i in range(total_vertex):
+    f.write("{},{}".format(i, len(total_map[i])))
+
+    for neighbor in total_map[i]:
+        f.write(",{}".format(neighbor))
+    
+    f.write("\n")
 
 f.close()
