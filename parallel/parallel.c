48e45acbdb131b51dda5ffdb26144e2bea754c57
Jingkang Wang <jingkanw@gmail.com>
2020-05-06 15:38:02 -0400

    Change get_min_color

--- parallel/parallel.c
+++ parallel/parallel.c
@@ -21,27 +21,38 @@ void assign_color(graph_t *g) {
     }
 }
 
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
 int get_min_color(graph_t *g, int vid) {
-    
-    bool color_valid = false;
-    int min_color = 0;
-    
-
-    while (!color_valid) {
-        min_color++;
-        color_valid = true;
-        int i;
-        for (i = 0; i < g->vertex[vid].degree; i++) {
-            int neighbor = g->vertex[vid].neighbor[i];
-
-            if (min_color == g->vertex[neighbor].color) {
-                color_valid = false;
-                break;
-            }
+    //printf("Get min color for %d!\n", vid);
+    int count = 0, i;
+    vertex_t v = g -> vertex[vid];
+    int nb_color_list[v.degree];
+    for (i = 0; i < v.degree; i++) {
+        int nb_id = v.neighbor[i];
+        vertex_t nb_v = g -> vertex[nb_id];
+        if (nb_v.is_colored){
+        	nb_color_list[count] = nb_v.color;
+        	count ++;
         }
     }
+    qsort(nb_color_list, count, sizeof(int), &cmpfunc);
 
-    return min_color;
+    // if no neighbor colored 1
+    if (nb_color_list[0] != 1){
+    	return 1;
+    } 
+    // chech least available color
+    // case 1: available color < max(nb_colors)
+    for (i = 1; i < count; i ++){
+    	if (nb_color_list[i] - nb_color_list[i - 1] >= 2){
+    		return nb_color_list[i-1] + 1;
+    	}
+    }
+    // case 2: availble color = max(nb_colors + 1)
+    return nb_color_list[count - 1] + 1; 
 }
 
 
@@ -110,11 +121,14 @@ int main(int argc, char *argv[]) {
 
     start = currentSeconds();
     assign_color(g);
-   
+
+    double assign_color_end = currentSeconds();
+
     unsigned char conflicts[g->nvertex];
     
     #pragma omp barrier
     detect_conflicts(g, conflicts);
+    double detect_conflicts_end = currentSeconds();
 
     #pragma omp barrier
     solve_conflicts(g, conflicts);
@@ -124,6 +138,9 @@ int main(int argc, char *argv[]) {
     duration = stop - start;
 
     printf("=============== Total running time is %f =============\n\n", duration);
+    printf("=============== assign_color running time is %f =============\n\n", assign_color_end - start);
+    printf("=============== detect_conflicts running time is %f =============\n\n", detect_conflicts_end - assign_color_end);
+    printf("=============== solve_conflicts running time is %f =============\n\n", stop - detect_conflicts_end);
     
     check_color(g);
 
--- parallel/parallel_optimized.c
+++ parallel/parallel_optimized.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include "../timer/cycletimer.h"
 
-#define INIT_SIZE 100
+#define INIT_SIZE 30
 
 void assign_color(graph_t *g);
 int get_min_color(graph_t *g, int vid);
@@ -66,27 +66,38 @@ void re_assign_color(graph_t *g, int *color_class_num, int **color_class_vertice
 }
 
 
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
 int get_min_color(graph_t *g, int vid) {
-    
-    bool color_valid = false;
-    int min_color = 0;
-    
-
-    while (!color_valid) {
-        min_color++;
-        color_valid = true;
-        int i;
-        for (i = 0; i < g->vertex[vid].degree; i++) {
-            int neighbor = g->vertex[vid].neighbor[i];
-
-            if (min_color == g->vertex[neighbor].color) {
-                color_valid = false;
-                break;
-            }
+    //printf("Get min color for %d!\n", vid);
+    int count = 0, i;
+    vertex_t v = g -> vertex[vid];
+    int nb_color_list[v.degree];
+    for (i = 0; i < v.degree; i++) {
+        int nb_id = v.neighbor[i];
+        vertex_t nb_v = g -> vertex[nb_id];
+        if (nb_v.is_colored){
+        	nb_color_list[count] = nb_v.color;
+        	count ++;
         }
     }
+    qsort(nb_color_list, count, sizeof(int), &cmpfunc);
 
-    return min_color;
+    // if no neighbor colored 1
+    if (nb_color_list[0] != 1){
+    	return 1;
+    } 
+    // chech least available color
+    // case 1: available color < max(nb_colors)
+    for (i = 1; i < count; i ++){
+    	if (nb_color_list[i] - nb_color_list[i - 1] >= 2){
+    		return nb_color_list[i-1] + 1;
+    	}
+    }
+    // case 2: availble color = max(nb_colors + 1)
+    return nb_color_list[count - 1] + 1; 
 }
 
 
@@ -151,11 +162,6 @@ int main(int argc, char *argv[]) {
 
     print_graph_info(g);
 
-    double duration;
-
-    start = currentSeconds();
-    assign_color(g);
-
     int *color_class_num;
     int **color_class_vertices;
     int color_max;
@@ -163,23 +169,35 @@ int main(int argc, char *argv[]) {
     color_class_num = (int *)calloc(2 * MAX_COLOR, sizeof(int));
     color_class_vertices = (int **)malloc(MAX_COLOR * sizeof(int *));
 
+    double duration;
+
+    start = currentSeconds();
+    assign_color(g);
+    double assign_color_end = currentSeconds();
+
     color_max = split_color_class(g, color_class_num, color_class_vertices);
     #pragma omp barrier
     re_assign_color(g, color_class_num, color_class_vertices, color_max);
+    double re_assign_color_end = currentSeconds();
+
     unsigned char conflicts[g->nvertex];
     
     #pragma omp barrier
     detect_conflicts(g, conflicts);
+    double detect_conflicts_end = currentSeconds();
 
     #pragma omp barrier
     solve_conflicts(g, conflicts);
-
     stop = currentSeconds();
 
     duration = stop - start;
 
     printf("=============== Total running time is %f =============\n\n", duration);
-    
+    printf("=============== assign_color running time is %f =============\n\n", assign_color_end - start);
+    printf("=============== re_assign_color running time is %f =============\n\n", re_assign_color_end - assign_color_end);
+    printf("=============== detect_conflicts running time is %f =============\n\n", detect_conflicts_end - re_assign_color_end);
+    printf("=============== solve_conflicts running time is %f =============\n\n", stop - detect_conflicts_end);
+
     check_color(g);
 
     return EXIT_SUCCESS;
